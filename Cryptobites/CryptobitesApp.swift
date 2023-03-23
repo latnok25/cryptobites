1aadf87caf9fb47e809e838f4505cdfe795d94eb
Daniel <deromyoha@gmail.com>
2023-03-23 13:00:03 +0100

    Initial Commit

---
+++ Cryptobites/CryptobitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptobitesApp.swift
+//  Cryptobites
+//
+//  Created by VENOM on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptobitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
